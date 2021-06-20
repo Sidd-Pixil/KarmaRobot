e0407f3dbfe94c7b7fba88f87061b46247663011
Sidd <77623761+Sidd-Pixil@users.noreply.github.com>
2021-06-20 19:01:58 +0530

    Update animequiz.py

--- SaitamaRobot/modules/animequiz.py
+++ SaitamaRobot/modules/animequiz.py
@@ -9,8 +9,8 @@ from telegram.ext import CallbackContext, run_async
 @run_async
 def aq(update: Update, context: CallbackContext):
     args = context.args
-    update.effective_message.reply_text(random.choice(animequote_string.ANIMEQUOTE))
+    update.effective_message.reply_text(random.choice(animequiz_string.Animequiz))
 
-AQ_HANDLER = DisableAbleCommandHandler("animequiz", animequiz)
+AQ_HANDLER = DisableAbleCommandHandler("aq", aq)
 
 dispatcher.add_handler(AQ_HANDLER)
