b7de493389eb5280c054d2d5ac63beea48d1d62d
Sidd <77623761+Sidd-Pixil@users.noreply.github.com>
2021-06-20 18:57:34 +0530

    Update animequiz_string.py

--- SaitamaRobot/modules/animequiz_string.py
+++ SaitamaRobot/modules/animequiz_string.py
@@ -1,3 +1,3 @@
 Animequiz = (
       "[1/10]  What is the outermost wall protecting the human called? - Aot", "[4/10] In which collage ken keneki used to go? - Tokyo ghoul" , 
-    "[2/10]  What is Oikawa jersy number? - Haikyuu", "[3/10]  Who was the first crew mate luffy recruited? - One piece" , "[5/10]  What is the name of High School that Izuku midoriya goes to - MHA"  
+    "[2/10]  What is Oikawa jersy number? - Haikyuu", "[3/10]  Who was the first crew mate luffy recruited? - One piece" , "[5/10]  What is the name of High School that Izuku midoriya goes to - MHA" ) 
